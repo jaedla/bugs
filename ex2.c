da4713d7fe24bf9069d0112f4f07c2e14b9232e7
Jüri Aedla <juri.aedla@gmail.com>
2017-12-22 13:42:20 +0200

    ex2

---
+++ ex2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char argbuf[32];
+int win;
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("usage: ex1 argument\n");
+    return 1;
+  }
+  char *arg = argv[1];
+  int i;
+  for (i = 0; arg[i] != 0; i++)
+    argbuf[i] = arg[i];
+
+  if (win == 1852729719)
+    printf("win\n");
+  else
+    printf("lose\n");
+
+  return 0;
+}
